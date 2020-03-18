9c99eca92c41f1ea8e86c4b31dc64f7ea92bd565
bartek-krk <62296439+bartek-krk@users.noreply.github.com>
2020-03-18 10:07:27 +0100

    Update MS.py

--- MS.py
+++ MS.py
@@ -9,13 +9,13 @@ import numpy as np
 #Bartek Lukasik
 ##
 
-def lorentz6P(x, y0, amp1, cen1, wid1, amp2, cen2, wid2, amp3, cen3, wid3, amp4, cen4, wid4, amp5, cen5, wid5, amp6, cen6, wid6):
-    return y0+2*amp1/np.pi*wid1/(4*(x-cen1)**2+wid1**2)+\
-2*amp2/np.pi*wid2/(4*(x-cen2)**2+wid2**2)+\
-2*amp3/np.pi*wid3/(4*(x-cen3)**2+wid3**2)+\
-2*amp4/np.pi*wid4/(4*(x-cen4)**2+wid4**2)+\
-2*amp5/np.pi*wid5/(4*(x-cen5)**2+wid5**2)+\
-2*amp6/np.pi*wid6/(4*(x-cen6)**2+wid6**2)
+def lorentz6P(x, y0, A1, cen1, wid1, A2, cen2, wid2, A3, cen3, wid3, A4, cen4, wid4, A5, cen5, wid5, A6, cen6, wid6):
+    return y0+2*A1/np.pi*wid1/(4*(x-cen1)**2+wid1**2)+\
+2*A2/np.pi*wid2/(4*(x-cen2)**2+wid2**2)+\
+2*A3/np.pi*wid3/(4*(x-cen3)**2+wid3**2)+\
+2*A4/np.pi*wid4/(4*(x-cen4)**2+wid4**2)+\
+2*A5/np.pi*wid5/(4*(x-cen5)**2+wid5**2)+\
+2*A6/np.pi*wid6/(4*(x-cen6)**2+wid6**2)
 
 data = pd.read_csv("moss.csv", delimiter='_', names=['args','vals'])
 
@@ -27,20 +27,20 @@ for i in range(0,len(data)):
     args.append(data.iloc[i][0])
 #predicted parameters
 y0_pred = 4435
-amp1_pred = 3538
+A1_pred = 3538
 cen1_pred = 60
-amp2_pred = 3736
+A2_pred = 3736
 cen2_pred = 88
-amp3_pred = 3999
+A3_pred = 3999
 cen3_pred = 116
-amp4_pred = 3962
+A4_pred = 3962
 cen4_pred = 138
-amp5_pred = 3658
+A5_pred = 3658
 cen5_pred = 166
-amp6_pred = 3540
+A6_pred = 3540
 cen6_pred = 194
 
-popt, pcov = curve_fit(lorentz6P, args, vals, p0=[y0_pred, amp1_pred, cen1_pred, 1, amp2_pred, cen2_pred, 1, amp3_pred, cen3_pred, 1, amp4_pred, cen4_pred, 1, amp5_pred, cen5_pred, 1, amp6_pred, cen6_pred, 1])
+popt, pcov = curve_fit(lorentz6P, args, vals, p0=[y0_pred, A1_pred, cen1_pred, 1, A2_pred, cen2_pred, 1, A3_pred, cen3_pred, 1, A4_pred, cen4_pred, 1, A5_pred, cen5_pred, 1, A6_pred, cen6_pred, 1])
 #1 is default value for p0 in curve_fit
 
 plt.figure(figsize=(20,10))
@@ -56,7 +56,7 @@ print("y0:", popt[0])
 j=1
 for i in range(1,18,3):
     while j<7:
-        print("peak amplitude ", j , "  ", popt[i])
+        print("area ", j , "  ", popt[i])
         j+=1
         break
 j=1    
@@ -79,7 +79,7 @@ j=1
 print("y0 error:", perr[0])
 for i in range(1,18,3):
     while j<7:
-        print("peak amplitude error ", j , "  ", perr[i])
+        print("area error ", j , "  ", perr[i])
         j+=1
         break
 j=1    
